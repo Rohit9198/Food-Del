1f27422a22fd1b715a0181753373314a131d1e26
ROHIT SINGH <rohitsi2252@gmail.com>
2026-09-05 19:24:18 +0530

    login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,13 +3,22 @@ import logo from "../../assets/admin_assets/logo.png"
 import search_icon from "../../assets/frontend_assets/search_icon.png"
 import basket_icon from "../../assets/frontend_assets/basket_icon.png"
 import './Navbar.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { StoreContext } from '../context/StoreContext'
+import { assets } from '../../assets/frontend_assets/assets'
 
 const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("menu");
 
-    const {getTotalCartAmount} = useContext(StoreContext);
+    const {getTotalCartAmount, token, setToken} = useContext(StoreContext);
+
+    const navigate = useNavigate();
+
+    const logout = () => {
+      localStorage.removeItem("token");
+      setToken("");
+      navigate("/")
+    }
   return (
     <div className='navbar'>
 
@@ -31,8 +40,15 @@ const Navbar = ({setShowLogin}) => {
           <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
 
-        <button onClick={() => setShowLogin(true)}>Sign in</button>
-
+        {!token?<button onClick={() => setShowLogin(true)}>Sign in</button>
+        :<div className='navbar-profile'>
+            <img src={assets.profile_icon} alt=""/>
+            <ul className="nav-profile-dropdown">
+              <li><img src={assets.bag_icon} alt=""/><p>Orders</p></li>
+              <hr/>
+              <li onClick={logout}><img src={assets.logout_icon} alt=""/><p>Logout</p></li>
+            </ul>
+        </div>}
       </div>
 
     </div>
